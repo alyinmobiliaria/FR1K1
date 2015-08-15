4c06e749dad688401694c66333035b45ce7fe9ee
ALY <alyinmobiliariasas@gmail.com>
2015-08-14 21:15:56 -0500

    CAJA DE COMENTARIOS
    - Esta lista la caja de comentarios y la vista de las noticias
    individuales.

---
+++ public/js/facebooksdk.js
@@ -0,0 +1,17 @@
+//Inicializando Facebook SDK
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '457274534458982',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
